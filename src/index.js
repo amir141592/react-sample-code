9ec51df7926c8342749979c00c0b6587c61a323e
amir141592 <amir.allahdadian@live.com>
2022-05-09 14:26:29 +0430

    some basic rules

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Main = () => {
+	return (
+		<main>
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+			<Section />
+		</main>
+	);
+};
+
+const Section = () => {
+	return (
+		<section className="book">
+			<Image />
+			<BookName />
+			<Description />
+		</section>
+	);
+};
+
+const Image = () => {
+	return (
+		<img
+			className="image"
+			src="https://images-na.ssl-images-amazon.com/images/I/81PNeyIYVfL._AC_UL127_SR127,127_.jpg"
+			alt="Greenlights"
+		/>
+	);
+};
+
+const BookName = () => {
+	return <h2 className="name">Greenlights</h2>;
+};
+
+const Description = () => {
+	return <p className="desc">A book about green lights</p>;
+};
+
+ReactDom.render(<Main />, document.getElementById("root"));
